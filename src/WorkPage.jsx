ed8d9206099022d6d16e7a887a5b7812bb68a545
Annabel McDonald <anna795@uw.edu>
2026-02-17 16:58:22 -0800

    Add colors and more projects to work page

--- src/WorkPage.jsx
+++ src/WorkPage.jsx
@@ -8,22 +8,50 @@ export function WorkPage(props) {
             <NavBar />
 
             <div className="work-section">
-                <h2> -- Portfolio --</h2>
+                <h2>- Projects -⁠</h2>
                 <div className="blurb-box">
                     <p className="blurb-text">blurb</p>
                 </div>
 
                 <div className="project-card">
                     <h3>ChoreSync</h3>
+                    <div className="break"></div>
                     <img src="/img/ChoreSync-MainPage.png" />
+                    <div className="break"></div>
                     <h4 className="tag web">Web Development</h4>
                     <h4 className="tag responsive">Responsive Design</h4>
                 </div>
 
                 <div className="project-card">
                     <h3>Pathways Forward</h3>
-                    <img src="/img/ChoreSync-MainPage.png" />
+                    <div className="break"></div>
+                    <img src="/img/PathwaysForward-Main.png" />
+                    <div className="break"></div>
                     <h4 className="tag research">User Research</h4>
+                    <h4 className="tag iterative">Iterative Process</h4>
+                    <h4 className="tag problem">Real-World Problem</h4>
+                    <h4 className="tag figma">Figma</h4>
+                    <h4 className="tag prototyping">Prototyping</h4>
+                    <h4 className="tag wireframing">Wireframing</h4>
+                </div>
+
+                <div className="project-card">
+                    <h3>OtterWise</h3>
+                    <div className="break"></div>
+                    <img src="/img/OtterWise-Logo.png" />
+                    <div className="break"></div>
+                    <h4 className="tag figma">Figma</h4>
+                    <h4 className="tag prototyping">Prototyping</h4>
+                    <h4 className="tag problem">Real-World Problem</h4>
+                </div>
+
+                <div className="project-card">
+                    <h3>This Website!</h3>
+                    <div className="break"></div>
+                    <img src="/img/OtterWise-Logo.png" />
+                    <div className="break"></div>
+                    <h4 className="tag web">Web Development</h4>
+                    <h4 className="tag iterative">Iterative Process</h4>
                     <h4 className="tag responsive">Responsive Design</h4>
                 </div>
             </div>
