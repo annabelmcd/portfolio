417ecec65ee009ae775a8f72df69d296629966e1
Annabel McDonald <anna795@uw.edu>
2026-03-11 15:44:52 -0700

    testing

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -10,7 +10,7 @@ export function AboutPage(props){
             <NavBar />
 
             <div className="about-section">
-                <img src='profilepic.jpg' className="profilepic" />
+                <img src='./img/profilepic.jpg' className="profilepic" />
                 <div className="blurb-box">
                     <p className="blurb-text">I’m a third year Informatics student at the University of Washington interested in product management and information systems, and I’m especially focused on understanding users and working closely with teams to solve meaningful problems. I bring strong collaboration, leadership, and communication skills, along with a systems-thinking mindset that helps connect user needs to practical solutions. I’m looking to continue developing my skills in a product role where I can learn from experienced teams and contribute to building impactful products.</p>
                 </div>
